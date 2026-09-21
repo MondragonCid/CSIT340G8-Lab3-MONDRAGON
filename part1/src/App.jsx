978581f81b781b14d40255c6cf73f1e933ae21a4
Mondragon <mondragonjacerrein@gmail.com>
2026-09-21 23:15:36 +0800

    1.3: Course Information step 3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,31 +1,26 @@
-
-const Content = (props) => {
-  return (
-    <div>
-      <p>
-        Part {props.part1}
-        Part {props.part2}
-        Part {props.part3}
-      </p>
-    </div>
-  )
-}
-
-
 const App = () => {
-  
-  const part1 = 'Fundamentals of React'
-  
-  const part2 = 'Using props to pass data'
-  
-  const part3 = 'State of a component'
-  
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
-      <Content part1 = {part1} />
-      <Content part2 = {part2} />
-      <Content part3 = {part3} />
+        <h1>{course}</h1>
+        <p>{part1.name} {part1.exercises}</p>
+        <p>{part2.name} {part2.exercises}</p>
+        <p>{part3.name} {part3.exercises}</p>
+
+        <p>Number of exercises : {part1.exercises + part2.exercises + part3.exercises} </p>
     </div>
   )
 }
